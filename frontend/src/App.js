a57c57c66c23c71e2bf74048afc4a646565205cd
Maurya Rajeev <104668455+mauryarajeev@users.noreply.github.com>
2023-10-08 20:19:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 // App.js
 import React from 'react';
-import Sidebar from './components/sideBar'; // Update the import path
-import Form from './components/Form'; // Update the import path
-import './App.css'; // Import your CSS file for styling
+import Sidebar from './components/sideBar'; 
+import Form from './components/Form'; 
+import './App.css'; 
 
 function App() {
   return (
